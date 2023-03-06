29af468f522ff151c437386627b4fc4a25beff5a
pf274 <peterfullmer33@gmail.com>
2023-03-06 12:54:39 -0700

    Continued work on sequence following

--- play.js
+++ play.js
@@ -1,15 +1,15 @@
 const button_info = {
     "button-top-left": {
-        pitch: 1,
+        tone: "c",
     },
     "button-top-right": {
-        pitch: 0.8,
+        tone: "d",
     },
     "button-bottom-left": {
-        pitch: 1.2,
+        tone: "f",
     },
     "button-bottom-right": {
-        pitch: 1.5,
+        tone: "a",
     },
 }
 
@@ -31,69 +31,105 @@ function getLighterColor(color) {
     return lighter_color;
 }
 
-function playSound(pitch) {
-    const audioContext = new AudioContext();
-    const audio = new Audio("tone.mp3");
-    const source = audioContext.createMediaElementSource(audio);
-    
-    // create a PitchShift node and connect it to the audio graph
-    const pitchShift = audioContext.createScriptProcessor(2048, 1, 1);
-    source.connect(pitchShift);
-    pitchShift.connect(audioContext.destination);
-    
-    // set the pitch shift parameter
-    pitchShift.onaudioprocess = function(event) {
-      const inputBuffer = event.inputBuffer;
-      const outputBuffer = event.outputBuffer;
-      for (let channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
-        const inputData = inputBuffer.getChannelData(channel);
-        const outputData = outputBuffer.getChannelData(channel);
-        for (let i = 0; i < inputData.length; i++) {
-          outputData[i] = inputData[i] * pitch;
-        }
-      }
-    };
-}
-
-function doSimonButton(button) {
-    // sound
-    debugger;
-    playSound(button_info[button.className].pitch);
-    
-    // color
-    let computedStyle = getComputedStyle(button);
-    let original_color = computedStyle.backgroundColor;
-    let new_color = getLighterColor(original_color);
-    button.style.backgroundColor = new_color;
-    setTimeout(() => {
-        button.style.backgroundColor = original_color;
-    }, 1000 * 0.3);
-}
-
 class Game {
     buttons;
     sequence;
+    state;
+    toneObjects;
+    player_sequence;
 
     constructor() {
         // buttons
         let buttonContainer = [...document.getElementsByClassName("button-container")][0];
         this.buttons = buttonContainer.children;
-
+        // sounds
+        this.toneObjects = {};
+        for (const tone of ["c", "d", "a", "f"]) {
+            let audioObject = new Audio(`tones/${tone}.wav`);
+            this.toneObjects[tone] = audioObject;
+        }
+        // sequence
         this.sequence = [];
+        this.player_sequence = [];
     }
 
     playSequence() {
+        this.state = "play";
         let new_button = this.buttons[Math.floor(Math.random() * (this.buttons.length))];
         this.sequence.push(new_button);
+        this.score = this.sequence.length;
         let remaining_sequence = [...this.sequence];
         let interval = setInterval(() => {
             let button = remaining_sequence.shift();
-            doSimonButton(button);
+            this.doSimonButton(button);
             if (remaining_sequence.length < 1) {
                 clearInterval(interval);
+                this.player_sequence = [];
+                this.state = "player";
             }
         }, 1000 * 0.4);
 
+    }
+
+    playSound(tone) {
+        const audio = this.toneObjects[tone];
+        // play the sound
+        audio.volume = 0.4;
+        audio.pause();
+        audio.currentTime = 0;
+        audio.play();
+    }
+
+    doSimonButton(button) {
+        // sound
+        this.playSound(button_info[button.className].tone);
+        
+        // color
+        let computedStyle = getComputedStyle(button);
+        let original_color = computedStyle.backgroundColor;
+        let new_color = getLighterColor(original_color);
+        button.style.backgroundColor = new_color;
+        setTimeout(() => {
+            button.style.backgroundColor = original_color;
+        }, 1000 * 0.3);
+    }
+
+    sendButtonInput(button) {
+        if (this.state === "player") {
+            this.doSimonButton(button);
+            let button_name = button.className;
+            console.log(button_name);
+            this.player_sequence.push(button);
+            let verified = this.verifyPlayerSequence();
+            if (verified) {
+                if (this.player_sequence.length >= this.sequence.length) {
+                    this.state = "new sequence"
+                    setTimeout( () => {
+                        this.playSequence();
+                    }, 1000);
+                }
+            } else {
+                // lost game!
+                this.restartGame();
+            }
+        }
+    }
+
+    verifyPlayerSequence() {
+        for (let i = 0; i < this.player_sequence.length; i++) {
+            if (this.player_sequence[i].className != this.sequence[i].className) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    restartGame() {
+        this.player_sequence = [];
+        this.sequence = [];
+        setTimeout(() => {
+            this.playSequence();
+        }, 1000 * 2);
     }
 }
 
@@ -102,5 +138,14 @@ const game = new Game();
 
 let resetButton = document.getElementById("resetButton");
 resetButton.addEventListener("click", () => {
-    game.playSequence();
+    game.restartGame();
 })
+
+let buttonContainer = [...document.getElementsByClassName("button-container")][0];
+let buttons = buttonContainer.children;
+
+for (const button of buttons) {
+    button.addEventListener("click", (event) => {
+        game.sendButtonInput(event.currentTarget);
+    })
+}
